6aadaf72f8466364188bad92be37d8ec44897249
Sean Beach <sean.beach1@yahoo.com>
2020-04-17 13:54:25 -0400

    css

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -7,15 +7,15 @@ class SignUp extends Component {
             <div class="ui form container">
         <form class="ui form" onSubmit = {this.props.handleSignupForm}> 
                 <label for="name">Name:</label>
-                <input type = 'text' id = 'name'onChange ={this.props.handleOnChangeForm }/><br></br>
+                <input class="signingUp" type = 'text' id = 'name'onChange ={this.props.handleOnChangeForm }/><br></br>
                 <label for="team"  >Favorite Team:</label>
-                <select onChange={this.props.handleOnChangeForm} class="ui selection dropdown">
+                <select class="signingUp" onChange={this.props.handleOnChangeForm} class="ui selection dropdown">
                 {this.props.teams.map(t => {
                     return <option id='team' value={t}>{t}</option>
                 })}
                 </select>
                 <label for="password">Password:</label>
-                <input type = 'password' id ='password' onChange ={this.props.handleOnChangeForm }/><br></br>
+                <input class="signingUp" type = 'password' id ='password' onChange ={this.props.handleOnChangeForm }/><br></br>
                 <br></br>
                 <button id="machineButtonGrid" class="ui button" type="submit" value="Submit">Submit</button>
         </form> 
